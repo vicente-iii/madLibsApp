f9af874281df0cb51373a5462b34f274ed6dc6c9
vicente-iii <vicente_iii@tangcueco.com.ph>
2022-04-12 02:00:57 +0800

    Added loop to display user prompts

--- app.js
+++ app.js
@@ -1,7 +1,22 @@
+//Questions
 let numberOfQuestions = 4;
-let userInputs = ["Splat Land", "Putin", "Crazed Goblin", "Goblinator"];
-let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[3]}`;
+let questionArray = ["Enter a venue", "Enter a name", "Enter a disgustign object", "Enter an event"]
 
+//Store user inputs
+let userInputs = [];
+let questionCounter = 0;
+for (let i = questionCounter; i < numberOfQuestions; i++) {
+    console.log(questionArray[i]);
+    userInputs.push(i);
+}
+
+console.log(userInputs)
+
+//Sample inputs
+//let userInputs = ["Splat Land", "Putin", "Crazed Goblin", "Goblinator"];
+
+//Crafting the Story
+let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[3]}`;
 console.log(originalStory);
 
 //'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}'
