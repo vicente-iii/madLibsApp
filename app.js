de3384df387f06c848f839e2df52ff87e0a4106a
vicente-iii <vicente_iii@tangcueco.com.ph>
2022-04-12 01:45:18 +0800

    Initialized story

---
+++ app.js
@@ -0,0 +1,6 @@
+let userInputs = ["Splat Land", "Putin", "Crazed Goblin"];
+let originalStory = `In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}`;
+
+console.log(originalStory);
+
+//'In ${userInputs[0]}, computer pioneer ${userInputs[1]} found herself working on a ${userInputs[2]}. Word got out that the team had "debugged" the ${userInputs[2]}'
